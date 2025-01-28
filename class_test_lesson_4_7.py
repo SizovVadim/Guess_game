54d3d3aca88fd4f2b8646bd083d0977569f89620
Vadim Sizov <vadim.sizovpgk@gmail.com>
2025-01-28 21:10:07 +0300

    add class lesson delete garbage

---
+++ class_test_lesson_4_7.py
@@ -0,0 +1,21 @@
+class Employee:
+    vacation_days = 28
+    def __init__(self, first_name_value, second_name_value, gender_value):
+        self.first_name = first_name_value
+        self.second_name = second_name_value
+        self.gender = gender_value
+
+# Создайте экземпляры класса Employee с различными значениями атрибутов.
+employee1 = Employee(first_name_value = 'Роберт',
+                    second_name_value = 'Крузо',
+                    gender_value = 'м'
+
+)
+employee2 = Employee(first_name_value = 'Роберт',
+                    second_name_value = 'Крузо',
+                    gender_value = 'м'
+
+)
+
+# Допишите код для вывода информации о сотрудниках.
+print(f'Имя: {employee1.first_name}, Фамилия: {employee1.second_name}, Пол: {employee1.gender}, Отпускных дней в году: {Employee.vacation_days}.')
--- type.py
+++
@@ -1,3 +0,0 @@
-def we_crash_all(name: bool):
-    return 'Привет, ' + str(name) + ', мы все сломали!'
-print(we_crash_all(True))
